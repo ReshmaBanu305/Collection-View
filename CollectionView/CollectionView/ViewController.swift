9d4fdfc8c18daf769ce76c1342ab248b636f3cf2
ReshmaBanu305 <reshmabanu.palagiri@gmail.com>
2024-08-04 17:39:16 +0530

    Add files via upload

---
+++ CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Admin on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+
+    @IBOutlet weak var homeCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        homeCollectionView.register(UINib(nibName: "homeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "homeCollectionViewCell")
+        homeCollectionView.delegate = self
+        homeCollectionView.dataSource = self
+    }
+
+
+}
+extension ViewController: UICollectionViewDelegate{
+    
+}
+extension ViewController: UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 4
+    }
+    //func indexTitles(for collectionView: UICollectionView) -> [String]? {
+       // return ["tree 1", "tree 2", "tree3", "tree4"]
+    //}
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = homeCollectionView.dequeueReusableCell(withReuseIdentifier: "homeCollectionViewCell", for: indexPath)
+        return cell
+    }
+    
+  
+}
---
+++ CollectionView/CollectionView/homeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  homeCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Admin on 6/10/24.
+//
+
+import UIKit
+
+class homeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
